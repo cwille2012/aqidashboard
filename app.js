93b59aa7d851994e5ce02eed368fd5821c18d712
Chris Wille <chris@christopherwille.com>
2018-01-28 09:09:51 -0500

    map update

--- app.js
+++ app.js
@@ -2235,6 +2235,92 @@ socket.on('open', function() {
 
 
 
+            //VISUALIZATION AND MAPPING HERE
+
+            //const DATA_URL = 'https://raw.githubusercontent.com/uber-common/deck.gl-data/master/examples/3d-heatmap/heatmap-data.csv';
+
+
+
+            class Root extends Component {
+                constructor(props) {
+                    super(props);
+                    this.state = {
+                        viewport: {
+                            ...DeckGLOverlay.defaultViewport,
+                            width: 500,
+                            height: 500
+                        },
+                        data: null
+                    };
+
+                    var response = new Array();
+                    for (var i in newData) {
+
+                        var lng = newData[i]['pos']['lon'];
+                        var lat = newData[i]['pos']['lat'];
+
+                        var positionArray = new Array();
+
+                        positionArray.push(lng);
+                        positionArray.push(lat);
+
+                        response.push(positionArray);
+
+                    }
+
+                    console.log(response);
+
+                    const data = response.map(d => [Number(d.lng), Number(d.lat)]);
+                    this.setState({ data });
+
+                }
+
+                componentDidMount() {
+                    window.addEventListener('resize', this._resize.bind(this));
+                    this._resize();
+                }
+
+                _resize() {
+                    this._onViewportChange({
+                        width: window.innerWidth,
+                        height: window.innerHeight
+                    });
+                }
+
+                _onViewportChange(viewport) {
+                    this.setState({
+                        viewport: {...this.state.viewport, ...viewport }
+                    });
+                }
+
+                render() {
+                    const { viewport, data } = this.state;
+
+                    return ( <
+                        MapGL {...viewport }
+                        mapStyle = "mapbox://styles/mapbox/dark-v9"
+                        onViewportChange = { this._onViewportChange.bind(this) }
+                        mapboxApiAccessToken = { MAPBOX_TOKEN } >
+                        <
+                        DeckGLOverlay viewport = { viewport }
+                        data = { data || [] }
+                        /> < /
+                        MapGL >
+                    );
+                }
+            }
+
+            render( < Root / > , document.getElementById('mapHolder').appendChild(document.createElement('div')));
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -2245,64 +2331,3 @@ socket.on('open', function() {
     });
     socket.on('close', function() {});
 });
-
-
-const DATA_URL =
-    'https://raw.githubusercontent.com/uber-common/deck.gl-data/master/examples/3d-heatmap/heatmap-data.csv';
-
-class Root extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            viewport: {
-                ...DeckGLOverlay.defaultViewport,
-                width: 500,
-                height: 500
-            },
-            data: null
-        };
-
-        requestCsv(DATA_URL, (error, response) => {
-            if (!error) {
-                const data = response.map(d => [Number(d.lng), Number(d.lat)]);
-                this.setState({ data });
-            }
-        });
-    }
-
-    componentDidMount() {
-        window.addEventListener('resize', this._resize.bind(this));
-        this._resize();
-    }
-
-    _resize() {
-        this._onViewportChange({
-            width: window.innerWidth,
-            height: window.innerHeight
-        });
-    }
-
-    _onViewportChange(viewport) {
-        this.setState({
-            viewport: {...this.state.viewport, ...viewport }
-        });
-    }
-
-    render() {
-        const { viewport, data } = this.state;
-
-        return ( <
-            MapGL {...viewport }
-            mapStyle = "mapbox://styles/mapbox/dark-v9"
-            onViewportChange = { this._onViewportChange.bind(this) }
-            mapboxApiAccessToken = { MAPBOX_TOKEN } >
-            <
-            DeckGLOverlay viewport = { viewport }
-            data = { data || [] }
-            /> < /
-            MapGL >
-        );
-    }
-}
-
-render( < Root / > , document.getElementById('mapHolder').appendChild(document.createElement('div')));
